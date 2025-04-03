3ec83700de6e28adedaf15b9698d9ef06996c139
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2025-04-03 12:56:00 -0300

    Adding D3

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,9 +2,65 @@
   <title>Projects</title>
 </svelte:head>
 
+<script>
+
+import Pie from '$lib/Pie.svelte';
+import projects from "$lib/projects.json";
+import Project from "$lib/Project.svelte";
+import * as d3 from 'd3';
+
+// let pieData = [
+//     { value: 1, label: "apples" },
+//     { value: 2, label: "oranges" },
+//     { value: 3, label: "mangos" },
+//     { value: 4, label: "pears" },
+//     { value: 5, label: "limes" },
+//     { value: 5, label: "cherries" }
+// ];
+let query = "";
+
+let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+
+// Make sure the variable definition is *outside* the block
+let pieData;
+
+    $: {
+        // Initialize to an empty object every time this runs
+        pieData = {};
+        
+        // Calculate rolledData and pieData based on filteredProjects here
+        let rolledData = d3.rollups(filteredProjects, v => v.length, d => d.year);
+
+        // We don't need 'let' anymore since we already defined pieData
+        pieData = rolledData.map(([year, count]) => {
+            return { value: count, label: year };
+        });
+    }
+
+// let filteredProjects;
+$: filteredProjects = projects.filter(project => {
+    let values = Object.values(project).join("\n").toLowerCase();
+    return values.includes(query.toLowerCase());
+});
+
+
+
+
+
+
+</script>
+
 <!-- Navigation menu: -->
 <h1>Projects</h1>
+<Pie data={pieData}/>
+
+<input type="search" bind:value={query} aria-label="Search projects" placeholder="🔍 Search projects..." />
+
+
+
+
 <div class="projects">
+
     <article>
         <h2>Lorem ipsum dolor</h2>
         <img src="https://www.svgrepo.com/show/340721/no-image.svg" alt="No image">
@@ -36,3 +92,4 @@
         <p>text</p>
     </article>
 </div>
+
